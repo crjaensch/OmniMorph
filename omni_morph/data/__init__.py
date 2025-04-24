158ddd8545f0301af2e38c3f7bfb3a0e46dd5116
crjaensch <crjaensch@users.noreply.github.com>
2025-04-24 18:26:44 +0200

    Minor update to module spec

--- omni_morph/data/__init__.py
+++ omni_morph/data/__init__.py
@@ -8,5 +8,6 @@ Data handling modules for OmniMorph.
 from .formats import Format
 from .converter import read, write, convert
 from .extractor import head, tail, sample
+from .statistics import get_stats
 
-__all__ = ["Format", "read", "write", "convert", "head", "tail", "sample"]
+__all__ = ["Format", "read", "write", "convert", "head", "tail", "sample", "get_stats"]
